bfce2772b7725b5bab8dd869261417869238bfbf
Vasanth9 <cheepurupalli.vasanthkumar@gmail.com>
2021-01-02 18:10:26 +0530

    stopwatch

--- src/App.js
+++ src/App.js
@@ -1,43 +1,66 @@
-import React ,{useState,useEffect}from 'react';
-import './App.css'
+import React, { useState ,useEffect} from 'react';
+import './App.css';
 
 export default function App() {
-    
-    const [hours,setHours]=useState("00");
-    const [minutes,setMinutes]=useState("00");
-    const [seconds,setSeconds]=useState("00");
-    const [date,setDate]=useState("date");
-    
-    useEffect(
-()=>{
-    const intervalId= setInterval(()=>{
-    let d=new Date();
-    var h=d.getHours();
-    var m=d.getMinutes().toString();
-    var s=d.getSeconds().toString();
-    h=(h%12)||12;
-    h=h.toString();
-    var z=d.getDate().toString().padStart(2,'0')+" / "
-    +(d.getMonth()+1).toString().padStart(2,'0')+" / "+d.getFullYear();
-    setDate(z);
-    setHours(h.padStart(2,'0'));
-    setMinutes(m.padStart(2,'0'));
-    setSeconds(s.padStart(2,'0'));
+
+   const [count,setCount]=useState(0);
+   const [seconds,setSeconds]=useState('00.00');
+   const [minutes,setMinutes]=useState('00');
+   const [start,setStart]=useState(false);
+   const [split,setSplit]=useState([])
+   
+
+   useEffect(() => {
+       let intervalId;
+       if(start){
+      intervalId=setInterval(()=>{
+           setCount(count=>count+1);
+           var sec=(count/100)
+           var min=Math.floor(sec/60)
+           sec=sec%60;
+
+           setSeconds(sec.toFixed(2).toString().padStart(5,'0'));
+           setMinutes(min.toString().padStart(2,'0'));
+      },10);
+    }
+       return () => {
+           clearInterval(intervalId);
+       }
+   }, [count,seconds,minutes,start])
     
 
-    return () => clearInterval(intervalId); 
-    },1000);
-},[seconds,minutes,hours,date]);
-   
-    return (
+    function handleSplit(){
+        
+        const add=minutes+":"+seconds;
+        if(split[split.length-1]!==add)
+        setSplit(split=>[...split,add]);
+        
+    }
+    function handleReset(){
+       
+        setCount(0);
+        setSplit([]);
+        setSeconds('00.00');
+        setMinutes('00');
+        setStart(false)
+        
+    }
+
+
+    return(
         <div>
-            <h1>Digital Clock</h1>
+            <h1>Stop Watch</h1>
             <hr/>
-            <h1 className="date">{date}</h1>
-           <h1 className="glow">{hours}
-           :{minutes}
-           :{seconds}</h1>
-           
+            <p className="stopwatch">{minutes}:{seconds}</p>
+            <center>
+                <button className="Reset" onClick={handleReset}>Reset</button>
+
+                <button  className="Split"onClick={handleSplit}>Split</button>
+                
+                <button className="Start"onClick={()=>setStart(!start)}>{start?'Pause':'Start'}</button>
+                </center>
+                <div >
+            {split.map(s=><p className="arr">{s}</p>)}</div>
         </div>
-    )
+    );
 }
