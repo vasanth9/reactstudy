afc5680a7e84635265db6843edfbe1c37980b6b5
Vasanth9 <cheepurupalli.vasanthkumar@gmail.com>
2021-01-21 22:19:01 +0530

    day016

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React,{useState}from 'react'
+import axios from "axios";
+
 
 export default function App() {
+    const [query,setQuery]=useState("");
+    const [bookList,setBookList]=useState([]);
+    const handleChange=e=>{
+        setQuery(e.target.value);
+    }
+    const handleSubmit=e=>{
+        e.preventDefault();
+        let q=`https://www.googleapis.com/books/v1/volumes?q=${query}&key=AIzaSyCND1fH_rCJvziOLdt5WG3GkHl6lDEXRf0`
+        axios.get(q).then(res=>{
+            
+            if(res.status===200)
+            {
+              
+                console.log(res.data.items)
+                setBookList(res.data.items)
+
+            }
+        })
+    }
     return (
         <div>
-            Book Deals
+           BookDeal
+           <form onSubmit={handleSubmit}>
+           <input type="text" onChange={handleChange} value={query} placeholder="Author, title Search" />
+           <button type="submit">Search</button>
+           </form>
+           <p>{query}</p>
+           <ul>{
+                bookList.map(item=>{return(<><li><img src={item.volumeInfo.imageLinks.smallThumbnail} alt={item.volumeInfo.title}/>{item.volumeInfo.title}</li></>)})
+               }
+             
+               </ul>
         </div>
     )
 }
