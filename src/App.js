1ef2729498b3cac713279640c9bc9086770aa576
Vasanth9 <cheepurupalli.vasanthkumar@gmail.com>
2021-01-22 21:36:39 +0530

    books

--- src/App.js
+++ src/App.js
@@ -1,40 +1,81 @@
-import React,{useState}from 'react'
-import axios from "axios";
-
-
+import React,{useState} from 'react'
+import './App.css'
+import axios from 'axios';
 export default function App() {
-    const [query,setQuery]=useState("");
-    const [bookList,setBookList]=useState([]);
-    const handleChange=e=>{
-        setQuery(e.target.value);
+
+   const [query,setQuery]=useState("");
+   const [loading,setLoading]=useState(false);
+   const [book,setBook]=useState([]);
+
+
+
+
+    const handleInput=e=>{
+        setQuery(e.target.value)
     }
     const handleSubmit=e=>{
         e.preventDefault();
-        let q=`https://www.googleapis.com/books/v1/volumes?q=${query}&key=AIzaSyCND1fH_rCJvziOLdt5WG3GkHl6lDEXRf0`
+        setLoading(true)
+        let q=`https://www.googleapis.com/books/v1/volumes?q=${query}`;
         axios.get(q).then(res=>{
             
             if(res.status===200)
             {
-              
-                console.log(res.data.items)
-                setBookList(res.data.items)
-
+                setBook(res.data.items);
+                console.log(book)
             }
-        })
+            else{
+                console.log(res);
+            }
+        });
+        setLoading(false)
+
+
     }
     return (
         <div>
-           BookDeal
-           <form onSubmit={handleSubmit}>
-           <input type="text" onChange={handleChange} value={query} placeholder="Author, title Search" />
-           <button type="submit">Search</button>
-           </form>
-           <p>{query}</p>
-           <ul>{
-                bookList.map(item=>{return(<><li><img src={item.volumeInfo.imageLinks.smallThumbnail} alt={item.volumeInfo.title}/>{item.volumeInfo.title}</li></>)})
-               }
-             
-               </ul>
+           <h1> Google Books</h1>
+           <div className="form">
+               <form onSubmit={handleSubmit}>
+                   <input type="text" 
+                   className="input"
+                   onChange={handleInput}
+                   placeholder="Search by Author or Title"
+                   value={query}
+                   ></input>
+                   <button type="submit" 
+                   className="submitButton"
+                   
+                   >Search</button>
+               </form>
+               
+           </div>
+           
+           {loading?<p>Loading.....</p>:""}
+           <div className="book-list">{
+                     book.map(
+                         item=>{return(
+                             <div className="card" key={item.id}>
+                                 <h1>{item.volumeInfo.title}</h1>
+                                 <img className="part2"
+                                 src={item.volumeInfo.imageLinks===undefined?"":
+                                 item.volumeInfo.imageLinks.smallThumbnail}
+                                 alt={item.volumeInfo.title}
+                                 />
+                                <div className="overview"> 
+                                 <h5 >{item.volumeInfo.description}</h5>
+                                 
+                                  <a href={item.volumeInfo.previewLink}
+                                target="_blank"
+                                ><button className="but pre">Preview</button></a>
+                                <a href={item.saleInfo.buyLink}
+                                target="_blank"
+                                ><button className="but buy">Buy</button></a>
+                               </div>
+                                 </div>)
+                         }
+                     )}
+           </div>
         </div>
-    )
+    );
 }
