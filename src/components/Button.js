d4d72f36903e5a1cba72b90acd46486a8049097d
Vasanth9 <cheepurupalli.vasanthkumar@gmail.com>
2021-02-06 12:28:20 +0530

    linked css

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './Button.css'
 export default function Button({imp,text,link}) {
     return (
         <div className={`button ${imp==='secondary'?'button___white':''}`}>
