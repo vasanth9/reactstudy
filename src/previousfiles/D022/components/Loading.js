fb1186fd4de648dccee9153c44634d3a40a44d4e
Vasanth9 <cheepurupalli.vasanthkumar@gmail.com>
2021-02-08 10:42:21 +0530

    day 022

---
+++ src/previousfiles/D022/components/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Loading() {
+    return (
+        <div className="Loading">
+            <h1>Loading...</h1>
+            
+        </div>
+    )
+}
