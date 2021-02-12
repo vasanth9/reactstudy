50fa3e275dbce80f7c4659f1556e3defb5cc1941
Vasanth9 <cheepurupalli.vasanthkumar@gmail.com>
2021-02-12 16:22:30 +0530

    moved D023 to previous files

---
+++ src/previousfiles/D023/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Review from './Components/Review'
+import './App.css'
+ function App() {
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>Our Reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review/>
+      </section>
+     
+    </main>
+     
+
+  )
+}
+
+export default App;
